8b9ba86cbbc488347b698563f3be79c242df72b9
Daniel López <lopezz@Macbook-Air-de-Lopezz.local>
2024-07-24 19:23:50 +0200

    cpp08✅

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lopezz <lopezz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 17:07:51 by lopezz            #+#    #+#             */
+/*   Updated: 2024/07/24 19:21:21 by lopezz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <list>
+#include <algorithm>
+
+template <typename T>
+class MutantStack : public std::stack< T, std::deque<T> >
+{
+	public:
+		MutantStack(): std::stack< T, std::deque<T> >() {};
+		
+		MutantStack(const MutantStack &copy)
+		{
+			*this = copy;
+		};
+		MutantStack &operator=(const MutantStack &assign)
+		{
+			if (this != &assign)
+				*this = assign;		
+			return *this;
+		};
+		
+		~MutantStack() {};
+
+		typedef typename std::deque<T>::iterator iterator;
+		iterator begin()
+		{
+			return this->c.begin();
+		}
+		iterator end()
+		{
+			return this->c.end();
+		}
+};
+
+// template <typename T>
+// class MutantStack : public std::stack< T, std::list<T> >
+// {
+// 	public:
+// 		MutantStack(): std::stack< T, std::list<T> >() {};
+		
+// 		MutantStack(const MutantStack &copy)
+// 		{
+// 			*this = copy;
+// 		};
+// 		MutantStack &operator=(const MutantStack &assign)
+// 		{
+// 			if (this != &assign)
+// 				*this = assign;		
+// 			return *this;
+// 		};
+		
+// 		~MutantStack() {};
+
+// 		typedef typename std::list<T>::iterator iterator;
+// 		iterator begin()
+// 		{
+// 			return this->c.begin();
+// 		}
+// 		iterator end()
+// 		{
+// 			return this->c.end();
+// 		}
+// };
+
+#endif
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,7 +6,35 @@
 /*   By: lopezz <lopezz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 12:38:18 by lopezz            #+#    #+#             */
-/*   Updated: 2024/07/24 12:38:19 by lopezz           ###   ########.fr       */
+/*   Updated: 2024/07/24 19:22:42 by lopezz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	mstack.push(0);
+	mstack.push(0);
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	return 0;
+}
